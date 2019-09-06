b7b8741f4264da3667ca840f7069b691e8028062
popa dorin <dorinmarian89@gmail.com>
2019-09-06 23:54:28 +0300

    doing classis;

---
+++ src/main/java/org/fasttrackit/Animal.java
@@ -0,0 +1,11 @@
+package org.fasttrackit;
+
+public class Animal {
+    String name;
+    String namaeOfFavoriteFoods;
+    String nameOfRecreationActivity;
+    int levelOfHealthStatus;
+    int levelOfHunger;
+    int moodLevel;
+
+}
---
+++ src/main/java/org/fasttrackit/AnimalFood.java
@@ -0,0 +1,7 @@
+package org.fasttrackit;
+
+public class AnimalFood {
+
+    String name;
+
+}
